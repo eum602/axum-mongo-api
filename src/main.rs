5ee2b4e278df63de94d2c1f59aa678f217b9548a
eum602 <eum602@gmail.com>
2022-12-31 20:35:04 -0500

    chore: add routes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use axum::{Router, Server};
+use axum::{routing::get, Router, Server};
 
 #[tokio::main]
 async fn main() {
     let server_address = ([127, 0, 0, 1], 8080).into();
     println!("server_address: http://{:?}/", server_address);
     Server::bind(&server_address);
-    let app = Router::new();
+    let app = Router::new()
+        .route("/", get(|| async { "Welcome to main page" }))
+        .route("/greetings", get(greet));
     Server::bind(&server_address)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn greet() -> &'static str {
+    "Hello world!"
 }

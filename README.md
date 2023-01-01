cebe2d9f27fb39316060fd17e5c7b4d404736435
eum602 <eum602@gmail.com>
2022-12-31 20:24:44 -0500

    chore: add basic server using axum

---
+++ README.md
@@ -0,0 +1,14 @@
+# Guidelines
+
+## doc commands
+
+```sh
+cargo doc --no-deps --open --document-private-items
+cargo test
+cargo add axum
+```
+
+## Notes
+
+- gRPC -> Rust library -> Tonic
+- Axum: Part of Tokio Project
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, Server};
+
+#[tokio::main]
+async fn main() {
+    let server_address = ([127, 0, 0, 1], 8080).into();
+    println!("server_address: http://{:?}/", server_address);
+    Server::bind(&server_address);
+    let app = Router::new();
+    Server::bind(&server_address)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
